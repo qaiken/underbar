6e003f4a00d69e268e94b1e353593b96bdfe0521
Quinton Aiken <quintonlaiken@gmail.com>
2015-05-30 11:00:37 -0500

    add reject function

--- src/underbar.js
+++ src/underbar.js
@@ -80,7 +80,6 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
-
     var result = [];
 
     _.each(collection,function(item) {
@@ -88,13 +87,15 @@
     });
 
     return result;
-
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection,function(item) {
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
