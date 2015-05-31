62b778cb4786b99f3bfae3175348fcd7ca10f556
Quinton Aiken <quintonlaiken@gmail.com>
2015-05-31 17:11:57 -0500

    add extend function

--- src/underbar.js
+++ src/underbar.js
@@ -240,6 +240,15 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var objs = [].slice.call(arguments,1);
+
+    _.each(objs,function(currObj) {
+      for(var k in currObj) {
+        obj[k] = currObj[k];
+      }
+    });
+
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
